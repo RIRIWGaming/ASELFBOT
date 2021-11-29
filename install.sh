d12c5dfef428e6e7325a18e7acc5a3c50511f7cc
R41H4N920 <79522358+RIRIWGaming@users.noreply.github.com>
2021-11-29 08:49:02 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -10,6 +10,30 @@ apt-get install wget
 apt-get install tesseract
 apt-get install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-npm install
+npm i
+npm i @adiwajshing/baileys@latest
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+NOW=`date "+%d.%m.%Y"`
+TIME=`date "+%H:%M"`
+echo Loading…| lolcat
+echo █▒▒▒▒▒▒▒▒▒| lolcat
+echo 10%| lolcat
+echo ███▒▒▒▒▒▒▒| lolcat
+echo 30%| lolcat
+echo █████▒▒▒▒▒| lolcat
+echo 50%| lolcat
+echo ███████▒▒▒| lolcat
+echo 100%| lolcat
+echo ██████████| lolcat
 
-echo "DONE INSTALLING ALL PACKAGE REQUIRE, RUN npm start"
+
+echo YOUTUBE : RIRIWGaming| lolcat
+echo INSTAGRAM : @ririwgaming| lolcat
+npm start
